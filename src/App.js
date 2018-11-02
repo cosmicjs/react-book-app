e1fdcdb2559e3778203a50a5c8715a24c48631f0
3ndrius <andrzejo1991@gmail.com>
2018-11-02 23:14:20 +0100

    Adding a file css
    Import font-awesome, adding font-family Setting first font css from
    typespiration and import other font to try

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      dd
+   
+     <h1>The<br /> Device</h1>
+
+<p>He showed me the hidden surveillance device he'd found in his car. I showed him my smartphone and welcomed him to the club.</p>
+
+
       </div>
     );
   }
