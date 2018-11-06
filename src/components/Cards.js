2176c74c26db6b5ad693ca5ec424153aa5c58cdd
3ndrius <andrzejo1991@gmail.com>
2018-11-06 13:17:24 +0100

    Adding css

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,20 +10,21 @@ const Cards = (props) => {
       {
           props.booksList.map((item, index) => {
               return(
-                <Link to={'/'+ item.slug } key={index}>
-                    <div className="card">
+                    <div className="card" key={index}>
                         <div className="overlay">
                         <img src={item.metadata.hero.url} alt=""/>
                        <div className="strip">
                        <MenuLikes />
                        </div>
                         </div>
+                        <Link to={'/'+ item.slug }>
                         <h2>{item.title}</h2>
                         <br/>
                         <div><b>Author:</b> {item.metadata.author.title}</div>
                         <div><b>Genres:</b> {item.metadata.category.title}</div>
+                        </Link>
                     </div>
-                </Link>
+             
               )
           })
       }
--- src/components/SinglePageCard.js
+++ src/components/SinglePageCard.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux';
 
+import { Link } from 'react-router-dom';
+
  function SinglePageCard(props) {
      
   return (
+  <div className="div">
     <div className="single-card">
-   <div className="book"> {props.book && <img src={props.book.metadata.hero.url} alt="" />}</div>
+   <div className="book"> {props.book && <img src={props.book.metadata.hero.url} alt="" />}
+   </div>
      <div className="book-text">
      <h1> {props.book && props.book.title}</h1>
-  { props.book ? <p  dangerouslySetInnerHTML={{__html:props.book.content}}></p> : <p> Loading </p> }
+  { props.book ? <p  dangerouslySetInnerHTML={{__html:props.book.content}}></p> : <p> Loading </p> } 
+  <Link to='/' className="btn-back">Go back</Link>
      </div>
+    
+     <Link to='/' className="single-card-category">
+     <div >
+     {props.book && props.book.metadata.category.title}
+     </div>
+     </Link>
     </div>
+  </div>
   )
 }
 const mapStateToProps = (state, ownProps) => {
