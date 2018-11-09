cc4ee60d01aaddb8795bf77f64f5ba0a449d9371
3ndrius <andrzejo1991@gmail.com>
2018-11-09 20:24:03 +0100

    Try to add vote functionality

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,88 @@
-// import React from 'react'
-// import { connect } from 'react-redux'
+ import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-// import MenuLikes from './MenuLikes';
-// const Cards = (props) => {
 
-//   return (
-//     <div className="cards">
+const Cards = (props) => {
+  
+    const  Save = (item) => {
+        props.hitLike(item);  
+      }
+      console.log('Likes',props.like);
+
+  return (
+  
+    <div className="cards">
+
+      {
+        props.booksList.map((item, index) => {
+              return(
+                    <div className="card" key={index}>
+                        <div className="overlay">
+                        <img src={item.metadata.hero.url} alt=""/>
+                       <div className="strip">
+                       <div className="like-menu">
+        <ul className="position">
+           <li onClick={()=> Save(index)}>
+            <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i><span className="vote">{item.options.slug_field}</span>
+
+            </li>
+            <li><i className="fa fa-bookmark-o fa-2x" aria-hidden="true"></i>
+</li>
+<li><i className="fa fa-share-square-o fa-2x" aria-hidden="true"></i>
+</li>
+        </ul>  
+    </div>
+                       </div>
+                        </div>
+                        <Link to={'/'+ item.slug }>
+                        <h2>{item.title}</h2>
+                        </Link>
+                        <br/>
+                        <Link to={'/author/' + item.metadata.author.slug}><div><b>Author:</b> {item.metadata.author.title}</div></Link>
+                        <Link to={'/categories/' + item.metadata.category.slug}><div><b>Genres:</b> {item.metadata.category.title}</div></Link> 
+                    </div>
+              )
+          })
+      }
+    </div>
+  )
+}
+const mapStateToProps= (state) => {
+    return{
+        like: state.likes
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return{
+        hitLike: (item) => {
+            dispatch({type:'HANDLE_LIKE', like: item })
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Cards);
+
+// import React, { Component } from 'react'
+// class Cards extends Component {
+// //     state = {
+// //         test: []
+// //     }
+//   Save = (item) => {
+//     this.setState({
+//         test: this.state.test.concat(item)
+//     })
+//   }
+//   componentDidUpdate = (item) =>{
+//     console.log("Did update", item);
+//     this.props.hitLike(item);
+//   }
+
+//   render() {
+  
+//     return (
+//           <div className="cards">
 
 //       {
-//           props.booksList.map((item, index) => {
+//           this.props.booksList.map((item, index) => {
 //               return(
 //                     <div className="card" key={index}>
 //                         <div className="overlay">
@@ -16,7 +90,7 @@ import { Link } from 'react-router-dom'
 //                        <div className="strip">
 //                        <div className="like-menu">
 //         <ul>
-//             <li>
+//             <li onClick={()=> this.props.hitLike(item.slug)}>
 //             <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i>
 
 //             </li>
@@ -39,56 +113,14 @@ import { Link } from 'react-router-dom'
 //           })
 //       }
 //     </div>
-//   )
+//     )
+//   }
 // }
-// export default (Cards);
-
-import React, { Component } from 'react'
-
-export default class Cards extends Component {
-
-  Save = (item) => {
-    localStorage.setItem("Like", item );
-    console.log("Clicked", item);
-  }
-
-  render() {
-  
-    return (
-          <div className="cards">
-
-      {
-          this.props.booksList.map((item, index) => {
-              return(
-                    <div className="card" key={index}>
-                        <div className="overlay">
-                        <img src={item.metadata.hero.url} alt=""/>
-                       <div className="strip">
-                       <div className="like-menu">
-        <ul>
-            <li onClick={()=> this.Save(item.slug)}>
-            <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i>
-
-            </li>
-            <li><i className="fa fa-bookmark-o fa-2x" aria-hidden="true"></i>
-</li>
-<li><i className="fa fa-share-square-o fa-2x" aria-hidden="true"></i>
-</li>
-        </ul>  
-    </div>
-                       </div>
-                        </div>
-                        <Link to={'/'+ item.slug }>
-                        <h2>{item.title}</h2>
-                        </Link>
-                        <br/>
-                        <Link to={'/author/' + item.metadata.author.slug}><div><b>Author:</b> {item.metadata.author.title}</div></Link>
-                        <Link to={'/categories/' + item.metadata.category.slug}><div><b>Genres:</b> {item.metadata.category.title}</div></Link> 
-                    </div>
-              )
-          })
-      }
-    </div>
-    )
-  }
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return{
+//         hitLike: (item) => {
+//             dispatch({type:'HANDLE_LIKE', like: item })
+//         }
+//     }
+// }
+// export default connect(null, mapDispatchToProps)(Cards);
