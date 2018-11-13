991b285cb689b3a0ab2a20bbf5793a67ef3e9ea2
3ndrius <andrzejo1991@gmail.com>
2018-11-13 18:32:56 +0100

    Adding sal js remove font import turn on pwa

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,28 +15,31 @@ const Cards = (props) => {
       {
         props.booksList.map((item, index) => {
               return(
-                    <div className="card" key={index}>
-                        <div className="overlay">
+                <div className="card" key={index} data-sal="zoom-out"
+                data-sal-delay="500"
+                data-sal-duration="1200"
+                data-sal-easing="ease-out-bounce">
+                    <div className="overlay">
                         <img src={item.metadata.hero.url} alt=""/>
-                       <div className="strip">
-                       <div className="like-menu">
-        <ul className="position">
-           <li onClick={()=> Save(index, item.slug)}>
-            <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i><span className="vote">{item.options.slug_field}</span>
-            </li>
-<li> <a href={`https://twitter.com/intent/tweet?text=${item.slug}/${item.created}/${item.metadata.author.slug}`}><i className="fa fa-share-square-o fa-2x" aria-hidden="true"></i></a>
-</li>
-        </ul>  
-    </div>
-                       </div>
+                        <div className="strip">
+                            <div className="like-menu">
+                                <ul className="position">
+                                    <li onClick={()=> Save(index, item.slug)} >
+                                    <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i><span className="vote">{item.options.slug_field}</span>
+                                    </li>
+                                    <li> <a href={`https://twitter.com/intent/tweet?text=${item.slug}/${item.created}/${item.metadata.author.slug}`}><i className="fa fa-share-square-o fa-2x" aria-hidden="true"></i></a>
+                </li>
+                                </ul>  
+                            </div>
                         </div>
-                        <Link to={'/'+ item.slug }>
-                        <h2>{item.title}</h2>
-                        </Link>
-                        <br/>
-                        <Link to={'/author/' + item.metadata.author.slug}><div><b>Author:</b> {item.metadata.author.title}</div></Link>
-                        <Link to={'/categories/' + item.metadata.category.slug}><div><b>Genres:</b> {item.metadata.category.title}</div></Link> 
                     </div>
+                <Link to={'/'+ item.slug }>
+                <h2>{item.title}</h2>
+                </Link>
+                <br/>
+                <Link to={'/author/' + item.metadata.author.slug}><div><b>Author:</b> {item.metadata.author.title}</div></Link>
+                <Link to={'/categories/' + item.metadata.category.slug}><div><b>Genres:</b> {item.metadata.category.title}</div></Link> 
+                </div>
               )
           })
       }
@@ -60,67 +63,3 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Cards);
-
-// import React, { Component } from 'react'
-// class Cards extends Component {
-// //     state = {
-// //         test: []
-// //     }
-//   Save = (item) => {
-//     this.setState({
-//         test: this.state.test.concat(item)
-//     })
-//   }
-//   componentDidUpdate = (item) =>{
-//     console.log("Did update", item);
-//     this.props.hitLike(item);
-//   }
-
-//   render() {
-  
-//     return (
-//           <div className="cards">
-
-//       {
-//           this.props.booksList.map((item, index) => {
-//               return(
-//                     <div className="card" key={index}>
-//                         <div className="overlay">
-//                         <img src={item.metadata.hero.url} alt=""/>
-//                        <div className="strip">
-//                        <div className="like-menu">
-//         <ul>
-//             <li onClick={()=> this.props.hitLike(item.slug)}>
-//             <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i>
-
-//             </li>
-//             <li><i className="fa fa-bookmark-o fa-2x" aria-hidden="true"></i>
-// </li>
-// <li><i className="fa fa-share-square-o fa-2x" aria-hidden="true"></i>
-// </li>
-//         </ul>  
-//     </div>
-//                        </div>
-//                         </div>
-//                         <Link to={'/'+ item.slug }>
-//                         <h2>{item.title}</h2>
-//                         </Link>
-//                         <br/>
-//                         <Link to={'/author/' + item.metadata.author.slug}><div><b>Author:</b> {item.metadata.author.title}</div></Link>
-//                         <Link to={'/categories/' + item.metadata.category.slug}><div><b>Genres:</b> {item.metadata.category.title}</div></Link> 
-//                     </div>
-//               )
-//           })
-//       }
-//     </div>
-//     )
-//   }
-// }
-// const mapDispatchToProps = (dispatch) => {
-//     return{
-//         hitLike: (item) => {
-//             dispatch({type:'HANDLE_LIKE', like: item })
-//         }
-//     }
-// }
-// export default connect(null, mapDispatchToProps)(Cards);
